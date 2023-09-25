835a4d80fd27bffe963f5af17fc0f62feaf9c639
shorya_goyal <shoryagoyal18@gmail.com>
2023-09-25 21:02:25 +0530

    Added the button to move the scrollbar to the right and left.

--- Components/Home/UpcomingMovies.js
+++ Components/Home/UpcomingMovies.js
@@ -9,34 +9,58 @@ function UpcomingMovies() {
     );
 
     return (
-        <div>
+        <div className="py-3">
             <SectionHeadingWithLink name="Upcoming movies" link="#" />
-            <div className="overflow-x-scroll whitespace-nowrap no-scrollbar py-5">
-                {upcomingMovieData === null ? (
-                    <div data-testid="upcomingMovieShimmer">
-                        {[...Array(12)].map((_, index) => (
-                            <div className="inline-block w-[15%] mr-[1%]">
-                                <TvMoviesCardShimmer key={index} />
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <div data-testid="upcomingMovies">
-                        {upcomingMovieData.results.map((movie) => (
-                            <div className="inline-block w-[15%] mr-[1%]">
-                                <TvMovieCard
-                                    image={movie.poster_path}
-                                    id={movie.id}
-                                    vote_average={movie.vote_average}
-                                    vote_count={movie.vote_count}
-                                    name={movie.title}
-                                    key={movie.id}
-                                    toLink={`/movie/${movie.id}`}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                )}
+            <div className="flex">
+                <div className="text-white w-[4%] flex justify-center items-center">
+                    <button
+                        className="px-[39%] py-[50%] rounded z-10 bg-slate-600"
+                        onClick={() => {
+                            const scrollbar = document.querySelector('#scrollbar');
+                            scrollbar.scrollLeft -= 400;
+                        }}
+                    >
+                        &lt;
+                    </button>
+                </div>
+                <div className="overflow-x-scroll whitespace-nowrap no-scrollbar py-5 w-[100%] mx-[-4%]" id="scrollbar">
+                    {upcomingMovieData === null ? (
+                        <div data-testid="upcomingMovieShimmer">
+                            {[...Array(12)].map((_, index) => (
+                                <div className="inline-block w-[15%] mr-[1%]">
+                                    <TvMoviesCardShimmer key={index} />
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <div data-testid="upcomingMovies">
+                            {upcomingMovieData.results.map((movie) => (
+                                <div className="inline-block w-[15%] mr-[1%]">
+                                    <TvMovieCard
+                                        image={movie.poster_path}
+                                        id={movie.id}
+                                        vote_average={movie.vote_average}
+                                        vote_count={movie.vote_count}
+                                        name={movie.title}
+                                        key={movie.id}
+                                        toLink={`/movie/${movie.id}`}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <div className="text-white w-[4%] flex justify-center items-center">
+                    <button
+                        className="px-[39%] py-[50%] rounded z-10 bg-slate-600"
+                        onClick={() => {
+                            const scrollbar = document.querySelector('#scrollbar');
+                            scrollbar.scrollLeft += 400;
+                        }}
+                    >
+                        &gt;
+                    </button>
+                </div>
             </div>
         </div>
     );
